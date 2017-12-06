990b9b9c70becc7eca2b984a5d90f790ba8db731
Max Z <max.zlotskiy@gmail.com>
2017-12-06 14:23:52 -0500

    wrote gcd, did not test

--- funky_town.js
+++ funky_town.js
@@ -3,3 +3,12 @@ function fibonacci(n) {
     else if(n == 1) return 1;
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
+//greatest common factor
+function gcd(a, b) {
+    var factor = Math.min(a, b);
+    while(a % factor != 0 && b % factor != 0) {
+	factor -= 1;
+    }
+    return factor;
+}
